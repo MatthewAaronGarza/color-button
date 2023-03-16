ec6172f530edfa6d8153871cbbef8d7b5ba5d416
Matthew Garza <59824561+MatthewAaronGarza@users.noreply.github.com>
2023-03-16 01:44:55 -0500

    Create var that will give random color

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let colorButton = document.getElementById('color-button')
 
-function getRandomColor () {
+function changeColorButton () {
+
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
     
+
 }
