6f18cd41db7f51877d8c4d13e73030b199c6ef2e
Matthew Garza <59824561+MatthewAaronGarza@users.noreply.github.com>
2023-03-16 01:48:48 -0500

    Create changeColorButton function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let colorButton = document.getElementById('color-button')
 
 function changeColorButton () {
-
+    //this var is a string that is a random color
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    
+
+    //now we add the string to the background color property of the color button
+    colorButton.style.backgroundColor = '#' + randomColor 
 
 }
+
+//now that we made the function we actually need to call it when we click on the color button, in this case it is a div.
